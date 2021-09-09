d11f40ed9ad1c1ac644b83f878859c4440f03b30
Kaushik-Sridhar <kaushiksridhar97@gmail.com>
2021-09-09 13:00:39 +0530

    Host join sample code

--- live-games/service/src/main/java/com/livegames/host_join/controller/HostJoinController.java
+++ live-games/service/src/main/java/com/livegames/host_join/controller/HostJoinController.java
@@ -1,14 +1,75 @@
 package com.livegames.host_join.controller;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import com.livegames.model.User;
+import com.livegames.model.User.MessageType;
+
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@RestController
+@Controller
 class HostJoinController {
+    HashMap<String, List<User>> map = new HashMap<String, List<User>>();
 
-    @GetMapping(value="/", produces = MediaType.TEXT_PLAIN_VALUE)
-    public String sample(){
+    @MessageMapping("/hostgame")
+    public User hostGame(@Payload User user, SimpMessageHeaderAccessor headerAccessor) {
+        String key = "123";
+        List<User> users = new ArrayList<>();
+        users.add(user);
+        map.put(key, users);
+        headerAccessor.getSessionAttributes().put("username", user.getSender());
+        return user;
+    }
+
+    @GetMapping(value = "/", produces = MediaType.TEXT_PLAIN_VALUE)
+    @ResponseBody
+    public String sample() {
         return "index";
+    }
+
+    @PostMapping("/validate/{key}")
+    @ResponseBody
+    public MessageType validate(@RequestBody User user, @PathVariable String key) {
+        if (map.getOrDefault(key, new ArrayList<User>()).size() == 0) {
+            return User.MessageType.INVALID;
+        }
+        if (map.get(key).size() >= 2) {
+            return User.MessageType.FULL;
+        }
+        return User.MessageType.VALID;
+    }
+
+    @MessageMapping("/joingame/{key}")
+    @SendTo("/topic/public")
+    public User joinGame(@Payload User user, @DestinationVariable String key,
+            SimpMessageHeaderAccessor headerAccessor) {
+        // Add username in web socket session
+        if (map.getOrDefault(key, new ArrayList<User>()).size() == 0) {
+            user.setContent("Invalid Link");
+            user.setType(User.MessageType.INVALID);
+            return user;
+        }
+        if (map.get(key).size() >= 2) {
+            user.setType(User.MessageType.FULL);
+            return user;
+        }
+        List<User> users = map.get(key);
+        users.add(user);
+        map.put(key, users);
+        headerAccessor.getSessionAttributes().put("username", user.getSender());
+        return user;
     }
 }
--- live-games/ui/src/App.js
+++ live-games/ui/src/App.js
@@ -1,14 +1,156 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import SockJsClient from 'react-stomp';
 
-function App() {
-  return (
-    <div className="App">
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      enableSockClient: false,
+      key: null
+    }
+  }
+
+  clientRef = null;
+
+  onMessageReceived(payload) {
+    let message = payload;
+
+    let messageElement = document.createElement('li');
+    let username = document.querySelector('#name').value.trim();
+
+    if (message.type === 'JOIN') {
+      messageElement.classList.add('event-message');
+      message.content = message.sender + ' joined!';
+    } else if (message.type === 'LEAVE') {
+      messageElement.classList.add('event-message');
+      message.content = message.sender + ' left!';
+    } else if (message.type === 'INVALID') {
+      if (username === message.sender) {
+        messageElement.classList.add('event-message');
+        message.content = 'Invalid link';
+      }
+    } else {
+      if (username === message.sender) {
+        messageElement.classList.add('event-message');
+        message.content = 'Room is full';
+      }
+    }
+    let messageArea = document.querySelector('#messageArea');
+    let textElement = document.createElement('p');
+    let messageText = document.createTextNode(message.content);
+    textElement.appendChild(messageText);
+
+    messageElement.appendChild(textElement);
+
+    messageArea.appendChild(messageElement);
+    messageArea.scrollTop = messageArea.scrollHeight;
+  }
+
+  connect(event) {
+    let username = document.querySelector('#name').value.trim();
+    let key = document.querySelector('#link').value.trim();
+    let messageElement = document.createElement('li');
+    console.log(event);
+    if (username && key) {
+      const requestOptions = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ sender: username, type: 'JOIN' })
+      };
+      fetch('http://localhost:8080/validate/' + key, requestOptions)
+        .then(response => response.json())
+        .then(data => {
+          if (!(data === "INVALID" || data === "FULL")) {
+            this.setState({
+              enableSockClient: true,
+              key,
+              username
+            });
+          } else {
+            const message = {};
+            if (data === "INVALID") {
+              messageElement.classList.add('event-message');
+              message.content = 'Invalid link';
+            } else {
+              messageElement.classList.add('event-message');
+              message.content = 'Room full';
+            }
+            let messageArea = document.querySelector('#messageArea');
+            let textElement = document.createElement('p');
+            let messageText = document.createTextNode(message.content);
+            textElement.appendChild(messageText);
+
+            messageElement.appendChild(textElement);
+
+            messageArea.appendChild(messageElement);
+            messageArea.scrollTop = messageArea.scrollHeight;
+          }
+
+        });
+    }
+    event.preventDefault();
+  }
+
+  onConnect(key, username) {
+    if (key) {
+      console.log(this.clientRef);
+      this.clientRef.sendMessage("/app/joingame/" + key,
+        JSON.stringify({ sender: username, type: 'JOIN' })
+      )
+    } else {
+      console.log(this.clientRef);
+      this.clientRef.sendMessage("/app/hostgame",
+        JSON.stringify({ sender: username, type: 'JOIN' })
+      )
+    }
+  }
+
+  hostGame() {
+    let username = document.querySelector('#name').value.trim();
+    if (username) {
+      this.setState({
+        enableSockClient: true,
+        username: username
+      });
+    }
+  }
+
+  render() {
+    const { enableSockClient, key, username } = this.state;
+    return <div className="App">
+      {enableSockClient && <SockJsClient url='http://localhost:8080/ws' topics={['/topic/public']}
+        onMessage={this.onMessageReceived.bind(this)}
+        ref={(client) => { this.clientRef = client }}
+        onConnect={() => this.onConnect(key, username)} />}
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <form id="usernameForm" name="usernameForm">
+          <div className="form-group">
+            <input type="text" id="name" placeholder="Username" autoComplete="off" className="form-control" />
+            <input type="text" id="link" placeholder="Link" autoComplete="off" className="form-control" />
+          </div>
+          <div className="form-group">
+            <button
+              type="button"
+              className="accent username-submit"
+              onClick={this.hostGame.bind(this)}
+              name="host"
+              value="host">
+              Host Game
+            </button>
+            <button
+              type="button"
+              className="accent username-submit"
+              name="join"
+              onClick={this.connect.bind(this)}
+              value="join">
+              Join Game
+            </button>
+          </div>
+        </form>
+        <ul id="messageArea">
+
+        </ul>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -18,8 +160,8 @@ function App() {
           Learn React
         </a>
       </header>
-    </div>
-  );
+    </div>;
+  };
 }
 
 export default App;
